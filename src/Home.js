2029164bec569b9f5c8078476e59a7e6f616ce5d
becca09 <adeolaadefuye@gmail.com>
2022-07-23 03:19:52 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  console.log(blogs)
+
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
+}
+ 
+export default Home;
